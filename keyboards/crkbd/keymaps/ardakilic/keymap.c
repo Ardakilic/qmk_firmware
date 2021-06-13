bbdcfef98ac2c980f0ccaa0d134ba4e83631708d
Arda Kilicdagi <ardakilicdagi@gmail.com>
2021-06-13 03:12:36 +0300

    port breaking changes to layout

--- keyboards/crkbd/keymaps/ardakilic/keymap.c
+++ keyboards/crkbd/keymaps/ardakilic/keymap.c
@@ -231,7 +231,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_master) {
+  if (!is_keyboard_master()) {
     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
   }
   return rotation;
@@ -323,7 +323,7 @@ void oled_render_logo(void) {
 }
 
 void oled_task_user(void) {
-    if (is_master) {
+    if (is_keyboard_master()) {
         oled_render_layer_state();
         oled_render_keylog();
     } else {
