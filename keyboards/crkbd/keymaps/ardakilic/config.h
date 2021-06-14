a9abb70ea50b89b7e61134f048f27136e19d23d7
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2021-06-14 17:48:10 +0300

    further optimisations to layer

--- keyboards/crkbd/keymaps/ardakilic/config.h
+++ keyboards/crkbd/keymaps/ardakilic/config.h
@@ -34,6 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define TAPPING_TERM 100
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
