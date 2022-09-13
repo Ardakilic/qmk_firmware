6bd249e5927804b2c4cfe3901530689f5e6cb9c6
Arda Kilicdagi <ardakilicdagi@gmail.com>
2022-09-13 21:48:16 +0300

    removed rgb stuff, cleanup

--- keyboards/horizon/keymaps/ardakilic/rules.mk
+++ keyboards/horizon/keymaps/ardakilic/rules.mk
@@ -1,7 +1,8 @@
-PIMORONI_TRACKBALL_ENABLE = yes # internal variable
-
 POINTING_DEVICE_ENABLE = yes # Enable Pimoroni Trackball
 POINTING_DEVICE_DRIVER = pimoroni_trackball	
 
 # Enable mouse keys, left and right click etc.
 MOUSEKEY_ENABLE = yes
+
+# Debug mode
+# CONSOLE_ENABLE = yes
