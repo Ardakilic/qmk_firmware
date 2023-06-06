61225849a6419fc8c81ee99a4101bd50212595a8
Arda Kilicdagi <ardakilicdagi@gmail.com>
2023-06-06 21:39:06 +0300

    horizon updates for newer qmk versions

--- keyboards/horizon/keymaps/ardakilic_trackball/keymap.c
+++ keyboards/horizon/keymaps/ardakilic_trackball/keymap.c
@@ -53,7 +53,7 @@ enum horizon_layers {
 #define BACKTICK RALT(KC_BSLS)
 #define TILDE RALT(KC_RBRC)
 #define NUMBER_SIGN RALT(KC_3)
-#define LOCKSCREEN LCTL(LSFT(KC_POWER)) // Screen Lock shortcut for OSX
+#define LOCKSCREEN LCTL(LSFT(KC_PWR)) // Screen Lock shortcut for OSX
 
 /*
 // Unicode Turkish characters, in case it's needed
@@ -194,8 +194,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     LT(_NUMPAD, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    LSFT_T(KC_CAPS), KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_MS_BTN1, KC_MS_BTN1,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT /*KC_ENT*/,
-    LT(_SODA, KC_GRV), KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_BSLS), LT(_SPACE, KC_SPC), BALL_SCR, KC_MS_BTN2, KC_BSPC, LT(_RAISE, KC_NONUS_BSLASH), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    LSFT_T(KC_CAPS), KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_MS_BTN1, KC_MS_BTN1,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT) /*KC_ENT*/,
+    LT(_SODA, KC_GRV), KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_BSLS), LT(_SPACE, KC_SPC), BALL_SCR, KC_MS_BTN2, KC_BSPC, LT(_RAISE, KC_NONUS_BACKSLASH), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 ),
 
 /* Lower
@@ -213,7 +213,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LBRC, KC_RBRC, KC_EQL,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_MS_BTN1, KC_MS_BTN1,  KC_F12,  DOLLAR_SIGN, CURLY_OPEN, CURLY_CLOSE, KC_GRV, LSFT(KC_GRV),
-    _______, _______, _______, _______, _______, _______, BALL_SCR, KC_MS_BTN2, _______, _______,   SQUARE_OPEN, SQUARE_CLOSE, LSFT(KC_2), KC_NONUS_BSLASH
+    _______, _______, _______, _______, _______, _______, BALL_SCR, KC_MS_BTN2, _______, _______,   SQUARE_OPEN, SQUARE_CLOSE, LSFT(KC_2), KC_NONUS_BACKSLASH
 ),
 
 
@@ -237,7 +237,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Soda Layer. Adjust and other stuff
  * ,-------------------------------------------------------------------------------------------------.
- * |EepRST| Reset| Debug|      |      |      |    TRACK    |      |      |  Up  |      |      |      |
+ * |EepRST|BOTLDR| Debug|Reboot|      |      |    TRACK    |      |      |  Up  |      |      |      |
  * |------+------+------+------+------+------+     BALL    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |             |      | Left | Down |Right |      | Mute |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -247,7 +247,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------------------------------------------------------------------------------------'
  */
 [_SODA] = LAYOUT(
-    EEP_RST, RESET,   DEBUG, _______, _______, _______, _______, _______, KC_UP, _______, _______, _______,
+    EE_CLR,  QK_BOOT,  DB_TOGG, QK_RBT, _______, _______, _______, _______, KC_UP, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_MUTE,
     _______, _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN1, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY,
     _______, _______, _______, _______, _______, _______, BALL_SCR, KC_MS_BTN2, _______, _______, _______, _______, KC_BRMD, KC_BRMU
