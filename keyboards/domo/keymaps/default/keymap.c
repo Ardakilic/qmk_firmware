a8efd3f5a50ac4e5870ffd6e07907bc9fa750c8e
Arda Kilicdagi <ardakilicdagi@gmail.com>
2024-07-09 02:44:32 +0300

    feature: attempot to port domo macropad to modern qmk keyboard json format

--- keyboards/domo/keymaps/default/keymap.c
+++ keyboards/domo/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_PGDOWN);
+            tap_code(KC_PGDN);
         } else {
             tap_code(KC_PGUP);
         }
@@ -41,20 +41,3 @@ bool oled_task_user(void) {
     return false;
 }
 #endif
-
-// debugging
-// void keyboard_post_init_user(void) {
-//     // Customise these values to desired behaviour
-//     debug_enable = true;
-//     debug_matrix = true;
-//     // debug_keyboard=true;
-//     // debug_mouse=true;
-// }
-
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//     // If console is enabled, it will print the matrix position and status of each key pressed
-// #ifdef CONSOLE_ENABLE
-//     uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
-// #endif
-//     return true;
-// }
